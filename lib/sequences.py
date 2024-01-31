e00531e92c637e21c2f4b372d0a3d94076cb2cd2
Yisel Cintora Vega <yiselcintoravega@gmail.com>
2024-01-30 21:31:15 -0500

    completed lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,29 @@
 #!/usr/bin/env python3
 
+
+
 def print_fibonacci(length):
-    pass
+    my_fibonacci = [0, 1]
+    a = 0
+    b = 1
+
+    if length <=0:
+        print([])
+        
+    
+    elif length == 1:
+            print([my_fibonacci[0]])
+
+    elif length >= 2:
+        for _ in range(length - 2):
+            my_fibonacci.append(a + b)
+            a, b = b, a + b
+        print(my_fibonacci)
+
+    
+        
+        
+
+    
+
+   
